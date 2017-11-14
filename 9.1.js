60533d33baeccf1cc859e40ecb648588cdbd1561
katapo <kata.pochec@gmail.com>
2017-11-14 11:48:06 +0100

    first commit

---
+++ 9.1.js
@@ -0,0 +1,17 @@
+function getTriangleArea (a, h) {
+  if (a > 0 && h > 0) {
+    return a * h / 2;
+  } else {
+    return 'Nieprawidłowe dane';
+  }
+}
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(20, 20);
+console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(-20, 0);
+console.log(triangle3Area);
